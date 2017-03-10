f96c0efc0f464af679797839e885e9df418adb12
Darsa Mahendrarajah <dmahendrarajah@zaizi.com>
2017-03-10 09:45:28 +0530

    Added_multiple alexa skill app

--- samples/src/main/java/stashhelper/StashSpeechletRequestStreamHandler.java
+++ samples/src/main/java/stashhelper/StashSpeechletRequestStreamHandler.java
@@ -10,6 +10,7 @@ public class StashSpeechletRequestStreamHandler extends SpeechletRequestStreamHa
 	private static final Set<String> suportedApplicationIds=new HashSet<>();
 	static{
 		suportedApplicationIds.add("amzn1.ask.skill.291ee3aa-1ab1-47ea-b8c3-3af5d56b6adb");
+		suportedApplicationIds.add("amzn1.ask.skill.df2f9c14-2b84-4b3b-a7be-67eed02c9eb7");
 	}
 	
 	public StashSpeechletRequestStreamHandler(){
